5c85c11ed89b0604b90f6b8c5fab81cfbd5a0190
shahid <shahid.ice.nstu@gmail.com>
2020-12-23 09:56:15 +0600

    Wnwrapping a Decorator

---
+++ python_cookbook/metaprogramming/93.py
@@ -0,0 +1,28 @@
+import time
+from functools import wraps
+
+
+def timethis(func):
+    """
+    Decorator that reports the execution time.
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(func.__name__, end-start)
+        return result
+    return wrapper
+
+
+@timethis
+def countdown(n):
+    """
+    counts down
+    """
+    while n > 0:
+        n -= 1
+    return None
+
+orig_countdown = countdown.__wrapped__
